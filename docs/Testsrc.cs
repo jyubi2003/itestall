d29d8de5291338e10a6b0814de1792cd83b661f5
yoshida <yoshida.hiro-02@jp.fujitsu.com>
2015-05-27 14:21:34 +0900

    Change Testsrc.cs

--- docs/Testsrc.cs
+++ docs/Testsrc.cs
@@ -4,7 +4,16 @@ namespace App1
 {
 	class Program
 		{
-		static void Main()
-		{}
+		static void Main(Object[] args_)
+		{
+            var i = arg_[0];
+
+            int v = func1(i);
+        }
+
+        protected int func1(int aInt)
+        {
+            return aInt;
+        }
 	}
 }
